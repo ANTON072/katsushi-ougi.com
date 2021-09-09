4b0644f57a2e6b9c91025f4582282c8e790550ef
Katsushi OUGI <bdbkx286@gmail.com>
2021-09-09 13:27:14 +0000

    add css

--- wp-content/themes/katsushi-ougi.com/functions.php
+++ wp-content/themes/katsushi-ougi.com/functions.php
@@ -3,11 +3,68 @@
 add_action(
   'wp_enqueue_scripts',
   function () {
+
     wp_enqueue_style(
       'google-font',
       'https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@400;700&display=swap',
       false,
       'v1.0.0'
     );
+
+    wp_enqueue_style(
+      'normalize',
+      'https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css',
+      false,
+      'v1.0.0'
+    );
+
+    wp_enqueue_style(
+      'variables',
+      get_template_directory_uri() . '/styles/variables.css',
+      false,
+      'v1.0.0'
+    );
+
+    wp_enqueue_style(
+      'base',
+      get_template_directory_uri() . '/styles/base.css',
+      false,
+      'v1.0.0'
+    );
+
+    wp_enqueue_style(
+      'theme',
+      get_template_directory_uri() . '/style.css',
+      array(
+        'google-font',
+        'normalize',
+        'variables',
+        'base',
+      ),
+      'v1.0.0'
+    );
+
+    wp_deregister_script( 'jquery' );
+
+    wp_enqueue_script(
+      'jquery',
+      'https://code.jquery.com/jquery-3.6.0.min.js',
+      false,
+      'v1.0.0',
+      true
+    );
+
+    wp_enqueue_script(
+      'theme',
+      get_template_directory_uri() . '/scripts/common.js',
+      array(
+        'jquery'
+      ),
+      'v1.0.0',
+      true
+    );
+
   }
 );
+
+add_theme_support( 'title-tag' );
---
+++ wp-content/themes/katsushi-ougi.com/header.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+	<head>
+		<meta charset="<?php bloginfo( 'charset' ); ?>">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+		<link rel="preconnect" href="https://fonts.googleapis.com">
+		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+		<?php wp_head(); ?>
+		<title><?php bloginfo( 'name' ); ?></title>
+	</head>
+	<body <?php body_class(); ?>>
+
+		<?php wp_body_open(); ?>
+
+		<div id="wrapper">
+
+		<header id="global_header" class="p-globalHeader">
+			<div class="p-globalHeader__contents">
+				<div class="p-globalHeader__head">
+					<h1><a href="<?php echo esc_url( home_url() ); ?>"><?php bloginfo( 'name' ); ?></a></h1>
+					<p><?php bloginfo( 'description' ); ?></p>
+				</div>
+				<ul class="p-globalHeader__nav">
+					<li><a href="<?php echo esc_url( home_url() ); ?>">home</a></li>
+					<li><a href="<?php echo esc_url( get_year_link( '' ) ); ?>">articles</a></li>
+					<li><a href="/about">about</a></li>
+				</ul>
+			</div>
+		</header>
+
+		<div id="main_contents">
