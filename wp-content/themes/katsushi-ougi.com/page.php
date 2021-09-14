5afd44524b19bdb556e36ee79af04b8c3e1bc439
Katsushi OUGI <bdbkx286@gmail.com>
2021-09-14 11:18:48 +0000

    modify css

--- wp-content/themes/katsushi-ougi.com/page.php
+++ wp-content/themes/katsushi-ougi.com/page.php
@@ -11,7 +11,7 @@
           <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
         </div>
 
-        <div class="p-articleBody">
+        <div class="p-articleBody u-text-link">
           <?php the_content(); ?>
         </div>
       </article>
